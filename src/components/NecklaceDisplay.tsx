a252ce5c71682783c6263a9ad402c9bbcbe56dc1
Baptiste Pauletto <baptistep.contact@gmail.com>
2025-10-05 13:20:33 -0400

    style(NecklaceDisplay): remove emoji from instructional text for a cleaner look

--- src/components/NecklaceDisplay.tsx
+++ src/components/NecklaceDisplay.tsx
@@ -301,14 +301,14 @@ const NecklaceDisplay: React.FC = () => {
       {/* Show removal tip when charms are placed and tip hasn't been shown yet */}
       {showRemovalTip && (
         <div className="removal-tip">
-          💡 TAP UN CHARM POUR LE RETIRER
+          TAP UN CHARM POUR LE RETIRER
         </div>
       )}
       
       {/* Add instruction when in placement mode and a charm is selected, but only if user hasn't placed a charm yet */}
       {isDrawerOpen && selectedCharmId && !hasPlacedCharm && (
         <div className="placement-instructions">
-          👇 TAP UN POINT POUR PLACER TON CHARM
+          TAP UN POINT POUR PLACER TON CHARM
         </div>
       )}
       
@@ -367,7 +367,7 @@ const NecklaceDisplay: React.FC = () => {
       {/* Show first-time interaction hint if never interacted with necklace */}
       {isMobile && !hasInteractedWithNecklace && !isDrawerOpen && (
         <div className="first-interaction-hint">
-          👆 TAP LE COLLIER POUR COMMENCER
+          TAP LE COLLIER POUR COMMENCER
         </div>
       )}
       
